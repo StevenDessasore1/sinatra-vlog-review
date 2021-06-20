76f9f55004313a03807ca123c949f55c7c332a9b
StevenDessasore <stevendessasoreba@gmail.com>
2021-06-20 12:35:20 -0400

    added foreign key

--- app/models/vlog.rb
+++ app/models/vlog.rb
@@ -1,2 +1,4 @@
 class Vlog < ActiveRecord::Base
+
+    belongs_to :user
 end
--- db/migrate/20210620082034_create_vlogs.rb
+++ db/migrate/20210620082034_create_vlogs.rb
@@ -2,9 +2,8 @@ class CreateVlogs < ActiveRecord::Migration[5.2]
   def change
     create_table :vlogs do |t|
       t.string :title
-      t.references :user, foreign_key: {on_delete: :cascade}
-      t.string :vlogger
-
+      t.integer :user_id
+      
       t.timestamps null: false
     end
   end

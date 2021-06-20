53e85fab4bfc9df8f211c3f9e07b90e85262163d
StevenDessasore <stevendessasoreba@gmail.com>
2021-06-20 12:58:35 -0400

    added 3 more users in seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
 #creating users in seed file
 
 
+#create 4 users
+
+User.create(name:"Steven", email: "steven@gmail.com", password: "password")
+User.create(name:"Demeris", email: "demeris@gmail.com", password: "password")
+User.create(name:"Amare", email: "amare@gmail.com", password: "password")
+User.create(name:"Maddy", email: "maddy@gmail.com", password: "password")

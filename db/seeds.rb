be16414e7890900cd7063b9e8908672b24468117
StevenDessasore <stevendessasoreba@gmail.com>
2021-06-20 18:54:00 -0400

    added foreign key

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@
 
 #create 4 users
 
-User.create(name:"Steven", email: "steven@gmail.com", password: "password")
-User.create(name:"Demeris", email: "demeris@gmail.com", password: "password")
-User.create(name:"Amare", email: "amare@gmail.com", password: "password")
-User.create(name:"Maddy", email: "maddy@gmail.com", password: "password")
+steven = User.create(name:"Steven", email: "steven@gmail.com", password: "password")
+demeris = User.create(name:"Demeris", email: "demeris@gmail.com", password: "password")
+amare = User.create(name:"Amare", email: "amare@gmail.com", password: "password")
+maddy = User.create(name:"Maddy", email: "maddy@gmail.com", password: "password")

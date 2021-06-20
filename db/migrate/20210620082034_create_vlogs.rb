ca3524d0d1d90641300d21bf4f925586982f4edf
StevenDessasore <stevendessasoreba@gmail.com>
2021-06-20 04:37:56 -0400

    added - has secure password line 1 in user.rb

--- db/migrate/20210620082034_create_vlogs.rb
+++ db/migrate/20210620082034_create_vlogs.rb
@@ -1,6 +1,9 @@
-class CreateVlogs < ActiveRecord::Migration
+class CreateVlogs < ActiveRecord::Migration[5.2]
   def change
     create_table :vlogs do |t|
+      t.string :title
+      t.references :user, foreign_key: {on_delete: :cascade}
+      t.string :vlogger
 
       t.timestamps null: false
     end
